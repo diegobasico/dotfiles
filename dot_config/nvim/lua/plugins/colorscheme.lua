2991f7a387987c0143c75961fb2fc11626fad4a7
diego <diego.ugaz13@gmail.com>
2025-07-01 20:47:58 -0500

    found sonokai

--- dot_config/nvim/lua/plugins/colorscheme.lua
+++ dot_config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,53 @@
 return {
+  { "ellisonleao/gruvbox.nvim" },
   {
     "Mofiqul/adwaita.nvim",
     name = "adwaita",
     lazy = false,
     priority = 1000,
+  },
+  {
+    "catppuccin/nvim",
+    name = "catppuccin",
+    priority = 1000,
+    opts = {
+      flavour = "macchiato",
+    },
+  },
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+  },
+  {
+    "folke/tokyonight.nvim",
+    name = "tokyonight",
+    lazy = false,
+    priority = 1000,
+    opts = { style = "night" },
+  },
+  {
+    "sainnhe/sonokai",
+    lazy = false,
+    priority = 1000,
     config = function()
-      vim.cmd.colorscheme("adwaita")
+      vim.g.sonokai_style = "default"
+      vim.g.sonokai_enable_italic = true
+    end,
+  },
+  {
+    "sainnhe/everforest",
+    lazy = false,
+    priority = 1000,
+    config = function()
+      vim.g.everforest_background = "hard"
+      vim.g.everforest_enable_italic = true
+      vim.g.everforest_transparent_background = 1
     end,
   },
   {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "adwaita",
+      colorscheme = "everforest",
     },
   },
 }
