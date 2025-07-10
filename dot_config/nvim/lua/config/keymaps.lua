eef8243d82b9f9523331916e774fb4853603d687
diegobasico <153402609+diegobasico@users.noreply.github.com>
2025-07-10 13:58:57 -0500

    syncing only nvim plugins

--- dot_config/nvim/lua/config/keymaps.lua
+++
@@ -1,5 +0,0 @@
--- Keymaps are automatically loaded on the VeryLazy event
--- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
--- Add any additional keymaps here
-vim.keymap.set({ 'i', 'v', 'c' }, '<C-ñ>', '<Esc>', { desc = 'Ctrl+ñ to Escape' })
-vim.keymap.set({ "n", "v" }, "<leader>y", '"+y', { desc = "Yank to system clipboard" })
