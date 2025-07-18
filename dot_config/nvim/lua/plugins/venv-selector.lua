a32336ff243aa8d38ce7702add3834aa4423c5fd
diego <diego.ugaz13@gmail.com>
2025-07-17 23:21:10 -0500

    new venv-selector branch

--- dot_config/nvim/lua/plugins/venv-selector.lua
+++ dot_config/nvim/lua/plugins/venv-selector.lua
@@ -6,14 +6,16 @@ return {
     "mfussenegger/nvim-dap-python",
   },
   event = "VeryLazy",
+  branch = "regexp",
   opts = {
     name = ".venv", -- optional: name of the venv to search for automatically
     auto_refresh = true,
     dap_enabled = true, -- makes it work with debugpy
-    lspconfig = true,   -- automatically configures LSP for selected venv
+    lspconfig = true, -- automatically configures LSP for selected venv
   },
   keys = {
     { "<leader>vs", "<cmd>VenvSelect<CR>", desc = "Select VirtualEnv" },
     { "<leader>vc", "<cmd>VenvSelectCached<CR>", desc = "Select Cached Venv" },
   },
+  ---@type venv-selector.Config
 }
