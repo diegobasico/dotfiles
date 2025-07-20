117b14caa45017dd451e5a6b39cfade9a5a0d36b
diego <diego.ugaz13@gmail.com>
2025-07-20 12:42:00 -0500

    added emoji picker

--- dot_config/nvim/lua/plugins/markdown.lua
+++ dot_config/nvim/lua/plugins/markdown.lua
@@ -3,13 +3,4 @@ return {
     "dhruvasagar/vim-table-mode",
     ft = "markdown",
   },
-  {
-    "iamcco/markdown-preview.nvim",
-    ft = "markdown",
-    build = "cd app && npm install",
-  },
-  {
-    "preservim/vim-markdown",
-    ft = "markdown",
-  },
 }
